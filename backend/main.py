f2bce8786aa53ac651ee83925b512404c7c142c3
Ayush Chaurasiya <chaurasiyaayush2006@gmail.com>
2026-08-30 22:26:13 +0530

    feat: add OpenCV image processing

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,16 @@
-print("FoodLens backend starte!")
+import cv2
+
+image = cv2.imread("data/test_food.jpg")
+
+if image is None:
+    print("Could not read image")
+else:
+    print("Image loaded successfully")
+    print("Original shape:", image.shape)
+
+    resized = cv2.resize(image, (640, 640))
+
+    cv2.imwrite("data/resized_food.jpg", resized)
+
+    print("Resized image saved successfully")
+    print("New shape:", resized.shape)
